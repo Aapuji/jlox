df5a140fc26cce8caa0b3b959c490cb9e1f97e50
Aapuji <om.robotics16@gmail.com>
2023-06-23 14:48:21 -0400

    Added `Expr` generation & visitation + printing Finished the generator for `Expr`. Created visitor system for working with all the classes. Added printing class for the AST (perhaps move to a "debug" package?)

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -14,7 +14,7 @@ public class GenerateAst {
 
     String outputDir = args[0];
     defineAst(outputDir, "Expr", Arrays.asList(
-      "Binary : Expr left, Token opertaor, Expr right",
+      "Binary : Expr left, Token operator, Expr right",
       "Grouping : Expr expression",
       "Literal : Object value",
       "Unary : Token operator, Expr right"
@@ -33,6 +33,12 @@ public class GenerateAst {
     writer.println();
     writer.println("abstract class " + baseName + " {");
 
+    defineVisitor(writer, baseName, types);
+
+    // The base accept() method.
+    writer.println();
+    writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+
     // The AST classes.
     for (String type : types) {
       String className = type.split(":")[0].trim();
@@ -44,12 +50,27 @@ public class GenerateAst {
     writer.close();
   }
 
+  private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+    writer.println("  interface Visitor<R> {");
+
+    for (String type : types) {
+      String typeName = type.split(":")[0].trim();
+      writer.println(
+        "    R visit" + typeName + baseName + "(" + 
+        typeName + " " + baseName.toLowerCase() + ");"
+      );
+    }
+
+    writer.println("  }");
+  }
+
   private static void defineType(
     PrintWriter writer,
     String baseName,
     String className,
     String fieldList
   ) {
+    writer.println();
     writer.println("  static class " + className + " extends " + baseName + " {");
 
     // Constructor.
@@ -57,11 +78,20 @@ public class GenerateAst {
 
     // Store parameters in fields.
     String[] fields = fieldList.split(",");
+    for (int i = 0; i < fields.length; i++) fields[i] = fields[i].trim(); // Trims each field
+
     for (String field : fields) {
       String name = field.split(" ")[1];
       writer.println("      this." + name + " = " + name + ";");
     }
 
+    writer.println("    }");
+
+    // Visitor pattern.
+    writer.println();
+    writer.println("    @Override");
+    writer.println("    <R> R accept(Visitor<R> visitor) {");
+    writer.println("      return visitor.visit" + className + baseName + "(this);");
     writer.println("    }");
 
     // Fields.
