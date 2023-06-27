ebb9fd64bb9a350989881c28675f9c4b94f13094
Aapuji <om.robotics16@gmail.com>
2023-06-26 21:30:11 -0400

    Started work evaluating exprs with `Interpreter`

---
+++ lox/Interpreter.java
@@ -0,0 +1,15 @@
+package lox;
+
+class Interpreter implements Expr.Visitor<Object> {
+  @Override
+  public Object visitLiteralExpr(Expr.Literal expr) {
+    return expr.value;
+  }
+
+  @Override
+  public Object visitGroupingExpr(Expr.Grouping expr) {
+    return evaluate(expr.expression);
+  }
+
+  
+}
