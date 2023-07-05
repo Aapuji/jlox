4152f930f248bb448164324a66e9422e2405a876
Aapuji <om.robotics16@gmail.com>
2023-07-05 16:22:20 -0400

    Added `if`, `while`, and `for` statements TODO: Add +=, -=, *=, /=, etc. TODO: Add Break and Continue. TODO: Add single quoted strings.

--- lox/Parser.java
+++ lox/Parser.java
@@ -1,6 +1,7 @@
 package lox;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static lox.TokenType.*;
@@ -8,10 +9,10 @@ import static lox.TokenType.*;
 // Recursive Descent Parser
 class Parser {
   private static class ParseError extends RuntimeException {};
-  private static Lox.Mode executionMode;
 
   private final List<Token> tokens;
   private int current = 0;
+  private Lox.Mode executionMode;
 
   public Parser(List<Token> tokens) {
     this.tokens = tokens;
@@ -36,10 +37,6 @@ class Parser {
     return statements;
   }
 
-  private Expr expression() {
-    return assignment();
-  }
-
   private Stmt declaration() {
     try {
       if (match(VAR)) return varDeclaration();
@@ -53,10 +50,76 @@ class Parser {
   }
 
   private Stmt statement() {
+    if (match(FOR)) return forStatement();
+    if (match(IF)) return ifStatement();
     if (match(PRINT)) return printStatement();
+    if (match(WHILE)) return whileStatement();
     if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
     return expressionStatement();
+  }
+
+  private Stmt forStatement() {
+    consume(LEFT_PAREN, "Expected '(' after 'for'.");
+
+    Stmt initializer;
+    if (match(SEMICOLON)) {
+      initializer = null;
+    } else if (match(VAR)) {
+      initializer = varDeclaration();
+    } else {
+      initializer = expressionStatement();
+    }
+
+    Expr condition = null;
+    if (!check(SEMICOLON)) {
+      condition = expression();
+    }
+    consume(SEMICOLON, "Expected ';' after loop condition.");
+
+    Expr increment = null;
+    if (!check(RIGHT_PAREN)) {
+      increment = expression();
+    }
+    consume(RIGHT_PAREN, "Expected ')' after 'for' clauses.");
+
+    Stmt body = statement();
+
+    if (increment != null) {
+      body = new Stmt.Block(
+        Arrays.asList(
+          body,
+          new Stmt.Expression(increment)
+        )
+      );
+    }
+
+    if (condition == null) {
+      condition = new Expr.Literal(true);
+    }
+    
+    body = new Stmt.While(condition, body);
+
+    if (initializer != null) {
+      body = new Stmt.Block(Arrays.asList(initializer, body));
+    }
+
+    return body;
+  }
+
+  private Stmt ifStatement() {
+    consume(LEFT_PAREN, "Expected '(' after 'if'.");
+
+    Expr condition = expression();
+    consume(RIGHT_PAREN, "Expected ')' after 'if' condition.");
+
+    Stmt thenBranch = statement();
+    Stmt elseBranch = null;
+    if (match(ELSE)) {
+      elseBranch = statement();
+    }
+
+    return new Stmt.If(condition, thenBranch, elseBranch);
   }
 
   private List<Stmt> block() {
@@ -89,6 +152,17 @@ class Parser {
     return new Stmt.Var(name, initializer);
   }
 
+  private Stmt whileStatement() {
+    consume(LEFT_PAREN, "Expected '(' after 'while'.");
+
+    Expr condition = expression();
+    consume(RIGHT_PAREN, "Expected ')' after 'while' condition.");
+
+    Stmt body = statement();
+    
+    return new Stmt.While(condition, body);
+  }
+
   private Stmt expressionStatement() {
     Expr expr = expression();
     
@@ -103,8 +177,12 @@ class Parser {
     return new Stmt.Expression(expr);
   }
 
+  private Expr expression() {
+    return assignment();
+  }
+
   private Expr assignment() {
-    Expr expr = equality();
+    Expr expr = or();
 
     if (match(EQUAL)) {
       Token equals = previous();
@@ -116,6 +194,30 @@ class Parser {
       }
 
       error(equals, "Invalid assignment target.");
+    }
+
+    return expr;
+  }
+
+  private Expr or() {
+    Expr expr = and();
+
+    while (match(OR)) {
+      Token operator = previous();
+      Expr right = and();
+      expr = new Expr.Logical(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr and() {
+    Expr expr = equality();
+
+    while (match(AND)) {
+      Token operator = previous();
+      Expr right = equality();
+      expr = new Expr.Logical(expr, operator, right);
     }
 
     return expr;
